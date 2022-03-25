f2ab88924ebde2e887dc1cd6f8b950dff702c759
tushar-hebbar18 <hebbar.tushar@gmail.com>
2022-03-25 21:12:54 +0000

    Spring-boot

---
+++ todo-list/todoJava/src/main/java/com/tusharJava/todoapp/TodoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tusharJava.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaApplication.class, args);
+	}
+
+}
